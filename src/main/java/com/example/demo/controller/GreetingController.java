82269a6e138d715432e08878630be6b026d4498d
Kaviya <kaviyasugumar8@gmail.com>
2026-03-26 12:48:42 +0530

    [Kaviya] UC7: Implemented update greeting functionality using PUT API

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -39,4 +39,11 @@ public class GreetingController {
     public List<Greeting> getAllGreetings() {
         return greetingService.getAllGreetings();
     }
+    @PutMapping("/update/{id}")
+    public Greeting updateGreeting(
+            @PathVariable Long id,
+            @RequestParam String message) {
+
+        return greetingService.updateGreeting(id, message);
+    }
 }
